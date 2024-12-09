012273c6567ce3b94a785e2ae8b32523fdf08f64
hansilverl <hannahsilverb@gmail.com>
2024-12-09 12:33:46 +0200

    remake files

---
+++ Ex2/Ex2/node.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+class Node {
+private:
+	std::string name;
+	std::vector<Node*> neighbors;
+public:
+	Node(const std::string& input);
+	void addNeighbor(Node* p_neighbor);
+	const std::vector<Node*>& getNeighbors();
+	bool nameIs(const std::string& input);
+	const std::string& getName() const;
+	void print();
+};
