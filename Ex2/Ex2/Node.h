91971aa289a398d4be176be0575c37a47201f731
hansilverl <hannahsilverb@gmail.com>
2024-12-09 10:48:19 +0200

    Add Node class with neighbors and replace #pragma once
    Removed #pragma once and added include guards. Included <string> and <vector> headers. Introduced Node class with private members for name and neighbors, and public methods for construction, adding neighbors, retrieving neighbors, name checking, and printing details.

--- Ex2/Ex2/Node.h
+++ Ex2/Ex2/Node.h
@@ -1,1 +1,15 @@
-#pragma once
+#include <string>
+#include <vector>
+
+class Node {
+private:
+	std::string name;
+	std::vector<Node*> neighbors;
+public:
+	Node(const std::string& input);
+	void addNeighbor(Node* p_neighbor);
+	const std::vector<Node*>& getNeighbors();
+	bool nameIs(const std::string& input);
+	bool getName(std::string& output);
+	void print();
+};
